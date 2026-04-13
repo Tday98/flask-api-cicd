c3c03bebd8fff121bc4afc55a6dbbf95909e3fd1
Tristan Day <tmd526@gmail.com>
2026-04-13 00:29:52 +0000

    ci-cd testing

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.12
+WORKDIR /usr/local/app
+
+# Install the application dependencies
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy in the source code
+COPY app.py ./
+EXPOSE 8080
+
+ENV FLASK_APP=app.py
+
+# Setup an app user so the container doesn't run as the root user
+RUN useradd app
+USER app
+
+CMD ["flask", "run", "--host", "0.0.0.0", "--port", "8080"]
---
+++ test/flasktest.py
@@ -0,0 +1,20 @@
+import pytest
+from app import as flask_app
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+def test_get_health(client):
+    response = client.get('/health')
+    assert response.status_code == 200
+    assert response.json == {"status": "healthy"}
+
+def test_get_hello(client):
+    response = client.get('/hello')
+    assert response.status_code == 200
+    assert response.json == {"message": "Hello from Flask!"}
